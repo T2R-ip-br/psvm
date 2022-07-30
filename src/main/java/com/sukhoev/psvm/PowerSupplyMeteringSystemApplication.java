ebd30c849cd6c5e79c823f4b52d37317ba11efc6
T2R-ip-br <vsukhoev@gmail.com>
2022-07-30 23:38:27 +0300

    Initial commit

---
+++ src/main/java/com/sukhoev/psvm/PowerSupplyMeteringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sukhoev.psvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerSupplyMeteringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerSupplyMeteringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sukhoev/psvm/PowerSupplyMeteringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sukhoev.psvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerSupplyMeteringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
