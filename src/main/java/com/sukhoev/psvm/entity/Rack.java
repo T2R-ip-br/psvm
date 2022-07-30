63be4991bb46918bf9c3781982f5323c56ae50d2
T2R-ip-br <vsukhoev@gmail.com>
2022-07-31 02:01:53 +0300

    Added Entity

---
+++ src/main/java/com/sukhoev/psvm/entity/Rack.java
@@ -0,0 +1,32 @@
+package com.sukhoev.psvm.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity(name = "rack")
+public class Rack {
+
+    @Id
+    @SequenceGenerator(
+            name = "rack_sequence",
+            sequenceName = "rack_sequence",
+            initialValue = 1,
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "rack_sequence"
+    )
+    private Long id;
+    private String nameRack;
+    // id_premises
+    // id_model_rack
+}
